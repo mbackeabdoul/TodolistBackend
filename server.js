04f44b52cbdb4c90dda300d118a86b704b4ecbbf
Abdou Khoudoss <khoudossmbacke18@gmail.com>
2024-12-19 15:48:21 +0000

    lllllllllll

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); 
 
 // Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
